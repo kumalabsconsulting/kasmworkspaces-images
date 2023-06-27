2405530c1d4f2cc460e2d5ea390c4f7dbc6d83b3
kumalabsconsulting <kumalabsconsulting@gmail.com>
2023-06-27 06:03:54 +0000

    Kuma-Kasm-jammy-desktop Ajout de kubectl

---
+++ src/ubuntu/install/kubernetes/install_kubectl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -ex
+# https://kubernetes.io/fr/docs/tasks/tools/install-kubectl/
+
+if grep -q "ID=debian" /etc/os-release; then
+  apt-get update && sudo apt-get install -y apt-transport-https
+  curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+  echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  apt-get update
+  apt-get install -y kubectl
+else
+  curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  mv ./kubectl /usr/local/bin/kubectl
+  kubectl version --client
+fi
