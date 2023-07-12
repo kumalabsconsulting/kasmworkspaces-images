5656951f6d9b75eb87ee8de2f39a4c6473cee611
kumalabsconsulting <kumalabsconsulting@gmail.com>
2023-07-12 12:16:12 +0000

    Ajout de packer dans l'image

---
+++ src/ubuntu/install/packer/install_packer.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/amd64/g')
+
+if [ "${ARCH}" == "arm64" ] ; then
+    echo "Terraform for arm64 currently not supported, skipping install"
+    exit 0
+fi
+
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install packer
