59ae16520072ac5a2b788cf2fb46fb44222a1746
kumalabsconsulting <kumalabsconsulting@gmail.com>
2023-07-12 15:25:04 +0000

    Fix apt install command

--- src/ubuntu/install/packer/install_packer.sh
+++ src/ubuntu/install/packer/install_packer.sh
@@ -8,6 +8,6 @@ if [ "${ARCH}" == "arm64" ] ; then
     exit 0
 fi
 
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install packer
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
+apt update && apt install -y packer
