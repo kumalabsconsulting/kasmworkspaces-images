c3572623aca0fd87454ebd8675f501f212dc7d60
kumalabsconsulting <kumalabsconsulting@gmail.com>
2023-07-25 12:39:56 +0000

    Add kns switcher

--- src/ubuntu/install/kubernetes/install_kubectl.sh
+++ src/ubuntu/install/kubernetes/install_kubectl.sh
@@ -6,11 +6,13 @@ if grep -q "ID=debian" /etc/os-release; then
   apt-get update && sudo apt-get install -y apt-transport-https
   curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
   echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+  curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && chmod +x $_
   apt-get update
   apt-get install -y kubectl
 else
   curl -LO https://dl.k8s.io/release/$(curl -Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
   chmod +x ./kubectl
   mv ./kubectl /usr/local/bin/kubectl
+  curl https://raw.githubusercontent.com/blendle/kns/master/bin/kns -o /usr/local/bin/kns && chmod +x $_
   kubectl version --client
 fi
