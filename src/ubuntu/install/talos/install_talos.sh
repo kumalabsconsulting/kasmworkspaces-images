3a2ebd844af7b3b5067cdfc9ba102ba2c9a7d44e
kumalabsconsulting <kumalabsconsulting@gmail.com>
2023-09-05 09:51:06 +0000

    fix talos install

--- src/ubuntu/install/talos/install_talos.sh
+++ src/ubuntu/install/talos/install_talos.sh
@@ -8,7 +8,8 @@ echo "
 ############# INSTALLING TALOS ######################
 
 "
+apt-get update && apt-get install -y sudo
 curl -sL https://talos.dev/install | sh
-
+apt-get remove -y sudo
 ## Fix ownership
 chown -R 1000:0 /home/kasm-user
