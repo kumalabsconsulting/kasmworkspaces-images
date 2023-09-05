3fc743d199b029f1575145f23002ce898581a042
kumalabsconsulting <kumalabsconsulting@gmail.com>
2023-09-05 13:13:01 +0000

    fix droits vault cmd

--- src/ubuntu/install/vault/install_vault.sh
+++ src/ubuntu/install/vault/install_vault.sh
@@ -13,6 +13,7 @@ apt-get update && apt-get install -y gpg
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt-get update && apt-get install -y vault
+cp /usr/bin/vault /usr/local/bin/vault
 
 
 ## Fix ownership
